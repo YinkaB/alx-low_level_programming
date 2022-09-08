fb09c467e2e19ce63f586a73825c585a78260047
YinkaB <yinkababatolu@yahoo.com>
2022-09-08 13:53:41 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description - printf ""
+ * Description - printf "with proper grammar...."
  * Return - 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+		return (0);
 }
