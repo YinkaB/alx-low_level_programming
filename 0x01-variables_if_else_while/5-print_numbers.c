33042b15e7794788cb57cd7e872491c99d688158
YinkaB <yinkababatolu@yahoo.com>
2022-09-10 08:47:35 -0700

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints digitt numbers
+ *
+ * Description: Program tha prints single digit num
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
