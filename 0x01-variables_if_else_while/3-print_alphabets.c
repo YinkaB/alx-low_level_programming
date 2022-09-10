846aa26640d095f911721c4a2628b84c20dac4b2
YinkaB <yinkababatolu@yahoo.com>
2022-09-10 08:06:16 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Description: print alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
