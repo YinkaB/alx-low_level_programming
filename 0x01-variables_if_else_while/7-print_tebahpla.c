4658802bf5be4a0c3119d936c40596a0d657c590
YinkaB <yinkababatolu@yahoo.com>
2022-09-10 09:08:07 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - print reverse alphabet
+ *
+ * Description: reverse alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
