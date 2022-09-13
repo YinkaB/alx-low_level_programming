50867b5a9bd3665ccdec75affaaaa5f13f6c6b0a
YinkaB <yinkababatolu@yahoo.com>
2022-09-13 16:17:32 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Description: a program that prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int a = 0;
+
+	for (a = 0; a < 8; a++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
