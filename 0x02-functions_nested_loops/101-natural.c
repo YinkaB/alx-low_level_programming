3c476f94fd32252ed984542a001abdcdf84ac4eb
YinkaB <yinkababatolu@yahoo.com>
2022-09-14 14:47:23 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - starting block
+ * Description: prints the sum of multiple of 3
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
