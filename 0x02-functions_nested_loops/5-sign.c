d4987aee0cae110baed2cecefaec8e72a1dd89df
YinkaB <yinkababatolu@yahoo.com>
2022-09-14 03:45:59 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a numbers sign
+ *
+ * @n: the number at which the sign will be printed
+ *
+ * Return: 1 else 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchat('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
